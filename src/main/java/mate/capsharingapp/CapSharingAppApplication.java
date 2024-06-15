193ea323121ca8f8717873e6e581ab16878c50a6
Bakhmet Ivan <bahmetvanya1290@gmail.com>
2024-06-15 23:37:04 +0300

    Start of project

---
+++ src/main/java/mate/capsharingapp/CapSharingAppApplication.java
@@ -0,0 +1,13 @@
+package mate.capsharingapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CapSharingAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CapSharingAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/mate/capsharingapp/CapSharingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package mate.capsharingapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CapSharingAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
